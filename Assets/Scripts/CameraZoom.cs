d1290e053609cbd847b2d024385be93eccd00548
Iain <i_stanford@hotmail.com>
2019-10-12 12:39:08 +0100

    Mousewheel Zoom Added

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    private float _zoomAmount = 0;
+    private float _maxToClamp = 10;
+    private float _rotSpeed = 10;
+
+    void Update()
+    {
+        _zoomAmount += Input.GetAxis("Mouse ScrollWheel");
+        _zoomAmount = Mathf.Clamp(_zoomAmount, -_maxToClamp, _maxToClamp);
+        var translate = Mathf.Min(Mathf.Abs(Input.GetAxis("Mouse ScrollWheel")), _maxToClamp - Mathf.Abs(_zoomAmount));
+        transform.Translate(0, 0, translate * _rotSpeed * Mathf.Sign(Input.GetAxis("Mouse ScrollWheel")));
+    }
+}
